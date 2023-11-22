a7444e46fe97ceb47992ef204f9c56f7a330872f
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-11-22 15:05:17 +0000

    fix: Split generations

--- config/jane/components.json.php
+++ config/jane/components.json.php
@@ -3,7 +3,7 @@
 return [
     'json-schema-file' => __DIR__ . '/../../api/components/json.schema.json',
     'root-class' => 'JsonComponent',
-    'namespace' => 'Lenra\\App\\Components',
-    'directory' => __DIR__ . '/../../generated/components',
+    'namespace' => 'Lenra\\App\\Components\\Json',
+    'directory' => __DIR__ . '/../../generated/components/json',
 ];
 ?>
--- config/jane/components.lenra.php
+++ config/jane/components.lenra.php
@@ -3,7 +3,7 @@
 return [
     'json-schema-file' => __DIR__ . '/../../api/components/lenra.schema.json',
     'root-class' => 'LenraComponent',
-    'namespace' => 'Lenra\\App\\Components',
-    'directory' => __DIR__ . '/../../generated/components',
+    'namespace' => 'Lenra\\App\\Components\\Lenra',
+    'directory' => __DIR__ . '/../../generated/components/lenra',
 ];
 ?>
