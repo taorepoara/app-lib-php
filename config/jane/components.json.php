09eaebf185be7649dab942190f2479ccd7b23f3e
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-11-22 15:02:38 +0000

    build: Prepare component generation

---
+++ config/jane/components.json.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'json-schema-file' => __DIR__ . '/../../api/components/json.schema.json',
+    'root-class' => 'JsonComponent',
+    'namespace' => 'Lenra\\App\\Components',
+    'directory' => __DIR__ . '/../../generated/components',
+];
+?>
---
+++ config/jane/components.lenra.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'json-schema-file' => __DIR__ . '/../../api/components/lenra.schema.json',
+    'root-class' => 'LenraComponent',
+    'namespace' => 'Lenra\\App\\Components',
+    'directory' => __DIR__ . '/../../generated/components',
+];
+?>
