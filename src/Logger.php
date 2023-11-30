6609cd1e766955213b8d5da505551b7cb154388a
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-11-30 11:37:09 +0000

    feat: Create logger

---
+++ src/Logger.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lenra\App;
+
+class Logger {
+    public static function log(string $message): void {
+        fwrite(STDOUT, microtime() . " - " . $message . "\n");
+    }
+}
--- src/Runner.php
+++ src/Runner.php
@@ -10,21 +10,29 @@ class Runner {
             throw new Exception('The request must be POST');
         }
         
+        Logger::log("Read input");
         // Takes raw data from the request
-        $json = file_get_contents('php://input');
+        $json = stream_get_contents(STDIN);
+        Logger::log("\\Read input");
         
+        Logger::log("json_decode");
         // Converts it into a PHP object
         $data = json_decode($json);
         // var_dump($data);
         // var_dump($data->view);
+        Logger::log("\\json_decode");
         
+        Logger::log("require_once 'handler.php'");
         require_once 'handler.php';
+        Logger::log("\\require_once 'handler.php'");
 
         try {
+            Logger::log("handleRequest");
             handleRequest($data);
+            Logger::log("\\handleRequest");
         }
         catch (\Exception $e) {
-            error_log("An error occured: " . $e->getMessage());
+            Logger::log("An error occured: " . $e->getMessage());
         }
     }
 }
